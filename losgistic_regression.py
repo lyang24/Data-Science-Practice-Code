52cf5e191b9b7994f52edbc2165eef020db94c55
Eric Yang <lanqingy@usc.edu>
2016-10-17 18:46:33 -0700

    improved version thanks to Kyle

--- losgistic_regression.py
+++ losgistic_regression.py
@@ -2,21 +2,37 @@ import pandas as pd
 import statsmodels.api as sm
 import math
 
-def loan_function():
-	cleanloansData = pd.read_csv('https://github.com/lyang24/Thinkful/blob/master/loansData_clean.csv')
-	cleanloansData['IR_TF'] = cleanloansData['Interest.Rate'].map(lambda x: 0 if x < 12 else 1)
-	cleanloansData['Intercept'] = 1.0
-	# ind_vars = list(cleanloansData.columns.values)
-	ind_vars = ['Amount.Requested', 'FICO.Range','Intercept' ]
-	logit = sm.Logit(cleanloansData['IR_TF'], cleanloansData[ind_vars])
-	result = logit.fit()
-	coeff = result.params
-	negcoeff = [ (-x) for x in coeff]
-	v = float(input("How much do you want to borrow "))
-	yz = float(input("what is your FICO score "))
-	p = 1/(1 + math.exp(negcoeff[2] + negcoeff[0] * yz - negcoeff[1] * v))
-        return  "the probility getting the loan is {}".format(p) 
-        if p > .7:
-            print("you will get the loan automatically")
-        else:
-            print("you will not get the loan automatically")
+#upgraded version - thanks to my mentor kyle
+
+# Train model
+def train_model(fname):
+  cleanloansData = pd.read_csv(fname)
+  cleanloansData['IR_TF'] = cleanloansData['Interest.Rate'].map(lambda x: 0 if x < 12 else 1)
+  cleanloansData['Intercept'] = 1.0
+  # ind_vars = list(cleanloansData.columns.values)
+  ind_vars = ['Amount.Requested', 'FICO.Range','Intercept' ]
+  logit = sm.Logit(cleanloansData['IR_TF'], cleanloansData[ind_vars])
+  result = logit.fit()
+  coeff = result.params
+	return coeff
+
+def run_model(coeff, v, yz):
+	p = 1.0/(1 + math.exp(coeff[2] + coeff[1] * yz - coeff[0] * v))
+	print(p)
+
+coeff = train_model('C:\\Users\Eric Yang\loansData_clean.csv')
+
+run_model(coeff, 1000, 750)
+
+# Expectations
+run_model(coeff, 10000000, 250) # Should reject!
+run_model(coeff, 10, 820) # Obviously accept!
+
+
+
+# Get use case of the model as input
+v = float(input("How much do you want to borrow "))
+yz = float(input("what is your FICO score "))
+
+run_model(coef, v, yz)
+     
